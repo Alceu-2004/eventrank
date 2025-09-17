fbb1a73674255f6ce00678de9a74075129f02400
Alceu-2004 <145071940+Alceu-2004@users.noreply.github.com>
2025-09-17 09:35:28 -0300

    Inserção de dados, Usuarios, para teste no H2.

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuarios (id, nome, email, senha, data_criacao) 
+VALUES (1, 'Alice', 'alice@email.com', '1234', CURRENT_TIMESTAMP);
+
+INSERT INTO usuarios (id, nome, email, senha, data_criacao) 
+VALUES (2, 'Bob', 'bob@email.com', 'abcd', CURRENT_TIMESTAMP);
